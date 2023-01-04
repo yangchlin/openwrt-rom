561a7a6536fc06acc88c609f08e5329167c21452
yangchlin <62425061+yangchlin@users.noreply.github.com>
2023-01-04 20:16:21 +0800

    Update openwrt-fast-auto-package21.02.sh

--- buildsh/openwrt-fast-auto-package21.02.sh
+++ buildsh/openwrt-fast-auto-package21.02.sh
@@ -21,7 +21,7 @@ function create_custom()
 #版本号,用日期
 #VERSION=$(TZ='Asia/Shanghai' date +%Y%m%d.%H%M)
 valtime=$(TZ='Asia/Shanghai' date '+%Y-%m-%d %H:%M')
-MYINFO="'编译日期 $valtime by tick-guo(https://github.com/tick-guo/openwrt-rom),恩山论坛(https://www.right.com.cn/forum/home.php?mod=space&uid=715099&do=thread&view=me&from=space)'"
+MYINFO="'编译日期 $valtime'"
 echo $MYINFO
 #自定义lan ip, 设置中国时区
 mkdir -p files/etc/uci-defaults
